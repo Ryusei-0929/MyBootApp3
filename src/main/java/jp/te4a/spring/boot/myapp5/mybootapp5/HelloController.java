2a69bc2e7f75f6f37c753153b62fa641ed988729
Ryusei-0929 <220033@jc-21.jp>
2025-05-20 12:41:56 +0900

    コミットメッセージ

--- src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/HelloController.java
@@ -2,8 +2,9 @@ package jp.te4a.spring.boot.myapp5.mybootapp5;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController{
@@ -11,6 +12,13 @@ public class HelloController{
     @RequestMapping("/")
     public String index(Model model){
         //return "this is Spring Boot sample"
+        model.addAttribute("msg","this is a setting message");
         return "index";
+    }
+    @RequestMapping(value="/post",method=RequestMethod.POST)
+    public ModelAndView postForm(@RequestParam("text1")String text1){
+         ModelAndView mv = new ModelAndView("index");
+         mv.addObject("msg","you write"+ text1 +"!!!" );
+        return mv;
     }
 }
