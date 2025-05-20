60ddf6829882d66392ba85b2c9b4b4ccc9aabd38
Ryusei-0929 <220033@jc-21.jp>
2025-05-20 11:34:03 +0900

    コミットメッセージ

---
+++ src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/HelloController.java
@@ -0,0 +1,16 @@
+package jp.te4a.spring.boot.myapp5.mybootapp5;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class HelloController{
+
+    @RequestMapping("/")
+    public String index(Model model){
+        //return "this is Spring Boot sample"
+        return "index";
+    }
+}
